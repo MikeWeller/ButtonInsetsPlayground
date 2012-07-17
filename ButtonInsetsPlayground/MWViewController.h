77bc3dcd58bab26b41146dd94054c5530689bde4
Michael Weller <mike@mikeweller.com>
2012-07-17 15:36:03 +0200

    First working version

--- ButtonInsetsPlayground/MWViewController.h
+++ ButtonInsetsPlayground/MWViewController.h
@@ -10,4 +10,5 @@
 
 @interface MWViewController : UIViewController
 
+
 @end
--- ButtonInsetsPlayground/MWViewController.m
+++ ButtonInsetsPlayground/MWViewController.m
@@ -8,27 +8,203 @@
 
 #import "MWViewController.h"
 
+/* Values must match up with segmented control indices */
+enum MWInsetSelection {
+	kMWInsetSelectionContentEdgeInsets = 0,
+	kMWInsetSelectionImageEdgeInsets = 1,
+	kMWInsetSelectionTitleEdgeInsets = 2
+};
+
 @interface MWViewController ()
+
+@property (strong, nonatomic) IBOutlet UIButton *button;
+@property (strong, nonatomic) IBOutlet UISegmentedControl *segmentedControl;
+@property (strong, nonatomic) IBOutlet UISlider *topSlider;
+@property (strong, nonatomic) IBOutlet UISlider *leftSlider;
+@property (strong, nonatomic) IBOutlet UISlider *bottomSlider;
+@property (strong, nonatomic) IBOutlet UISlider *rightSlider;
+
+@property (nonatomic, copy) NSString *observedPropertyName;
 
 @end
 
 @implementation MWViewController
 
-- (void)viewDidLoad
-{
-    [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+@synthesize button = _button;
+@synthesize segmentedControl = _segmentedControl;
+@synthesize topSlider = _topSlider;
+@synthesize leftSlider = _leftSlider;
+@synthesize bottomSlider = _bottomSlider;
+@synthesize rightSlider = _rightSlider;
+@synthesize observedPropertyName = _observedPropertyName;
+
+- (IBAction)selectedSegmentChanged:(id)sender {
+	[self switchToCurrentlySelectedInset];
 }
 
-- (void)viewDidUnload
+- (void)switchToCurrentlySelectedInset
 {
-    [super viewDidUnload];
-    // Release any retained subviews of the main view.
+	enum MWInsetSelection selection = self.segmentedControl.selectedSegmentIndex;
+	[self switchToInset:selection];
+}
+
+- (void)switchToInset:(enum MWInsetSelection)insetSelection
+{
+	[self unobserveButtonProperty];
+	NSString *propertyToObserve = [self propertyToObserveForInsetSelection:insetSelection];
+	[self observePropertyNamed:propertyToObserve];
+}
+
+- (void)unobserveButtonProperty
+{
+	if (self.observedPropertyName) {
+		[self.button removeObserver:self
+				 forKeyPath:self.observedPropertyName];
+		self.observedPropertyName = nil;
+	}
+}
+
+- (NSString *)propertyToObserveForInsetSelection:(enum MWInsetSelection)insetSelection
+{
+	switch (self.segmentedControl.selectedSegmentIndex) {
+		case kMWInsetSelectionContentEdgeInsets:
+			return @"contentEdgeInsets";
+
+		case kMWInsetSelectionImageEdgeInsets:
+			return @"imageEdgeInsets";
+
+		case kMWInsetSelectionTitleEdgeInsets:
+			return @"titleEdgeInsets";
+
+		default:
+			NSAssert(0, nil);
+			return nil;
+	}
+}
+
+- (void)observePropertyNamed:(NSString *)propertyName
+{
+	NSAssert(!self.observedPropertyName, nil);
+
+	[self.button addObserver:self
+		      forKeyPath:propertyName
+			 options:NSKeyValueObservingOptionInitial | NSKeyValueObservingOptionNew
+			 context:NULL];
+
+	self.observedPropertyName = propertyName;
+}
+
+- (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary *)change context:(void *)context
+{
+	id newValue = [change valueForKey:NSKeyValueChangeNewKey];
+	NSAssert([newValue isKindOfClass:NSValue.class], nil);
+	NSValue *wrappedInsetsValue = newValue;
+
+	UIEdgeInsets insets = [wrappedInsetsValue UIEdgeInsetsValue];
+	[self updateSlidersForInsets:insets];
+}
+
+- (void)updateSlidersForInsets:(UIEdgeInsets)insets
+{
+	self.topSlider.value = insets.top;
+	self.leftSlider.value = insets.left;
+	self.bottomSlider.value = insets.bottom;
+	self.rightSlider.value = insets.right;
+}
+
+- (IBAction)sliderChanged:(id)sender {
+	UIEdgeInsets insets = UIEdgeInsetsMake(self.topSlider.value,
+					       self.leftSlider.value,
+					       self.bottomSlider.value,
+					       self.rightSlider.value);
+
+	insets = [self pixelAlignedInsets:insets forScale:self.view.window.screen.scale];
+
+	[self setCurrentlySelectedInsetsTo:insets];
+}
+
+
+- (UIEdgeInsets)pixelAlignedInsets:(UIEdgeInsets)insets forScale:(CGFloat)scale
+{
+	if (scale == 0.0f) {
+		scale = [[UIScreen mainScreen] scale];
+	}
+
+	if (scale == 0.0f) {
+		scale = 1.0f;
+	}
+
+	UIEdgeInsets result = insets;
+
+	result.left = floorf(result.left * scale) / scale;
+	result.top = floorf(result.top * scale) / scale;
+	result.right = ceilf(result.right * scale) / scale;
+	result.bottom = ceilf(result.bottom * scale) / scale;
+
+	return result;
+}
+
+- (void)setCurrentlySelectedInsetsTo:(UIEdgeInsets)insets
+{
+	NSValue *value = [NSValue valueWithUIEdgeInsets:insets];
+	[self.button setValue:value forKeyPath:self.observedPropertyName];
+	[self sizeButtonToFit];
+
+#ifndef NDEBUG
+	NSLog(@"Current insets changed to: %@", NSStringFromUIEdgeInsets(insets));
+#endif
+}
+
+- (void)sizeButtonToFit
+{
+	CGSize size = [self.button sizeThatFits:CGSizeMake(CGFLOAT_MAX, CGFLOAT_MAX)];
+	self.button.frame = CGRectMake((self.view.bounds.size.width - size.width) / 2,
+				       self.button.frame.origin.y,
+				       size.width,
+				       size.height);
+}
+
+- (IBAction)resetButtonPressed:(id)sender {
+	[self setCurrentlySelectedInsetsTo:UIEdgeInsetsZero];
+}
+
+- (void)viewDidLoad
+{
+	[super viewDidLoad];
+
+	[self setupButton];
+	[self switchToCurrentlySelectedInset];
+	[self sizeButtonToFit];
+}
+
+- (void)setupButton
+{
+	self.button.clipsToBounds = YES;
+	[self.button setBackgroundImage:[self buttonBackgroundImage]
+			       forState:UIControlStateNormal];
+}
+
+- (UIImage *)buttonBackgroundImage
+{
+	UIImage *result = [UIImage imageNamed:@"buttonBackground.png"];
+	return [result stretchableImageWithLeftCapWidth:15 topCapHeight:27];
+}
+
+- (void)didReceiveMemoryWarning
+{
+	[super didReceiveMemoryWarning];
+
+	[self setButton:nil];
+	[self setSegmentedControl:nil];
+	[self setTopSlider:nil];
+	[self setLeftSlider:nil];
+	[self setBottomSlider:nil];
+	[self setRightSlider:nil];
 }
 
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
 {
-	return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
+	return interfaceOrientation == UIInterfaceOrientationPortrait;
 }
 
 @end
